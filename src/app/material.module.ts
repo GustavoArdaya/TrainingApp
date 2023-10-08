2d49938800dd1aaae44f2349b0b043ca75676748
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-10-08 11:22:16 +0200

    add sidenav and toolbar

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -6,6 +6,9 @@ import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker'
 import { MatNativeDateModule } from "@angular/material/core";
 import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
+
 
 const MaterialModules = [
     MatButtonModule,
@@ -14,7 +17,9 @@ const MaterialModules = [
     MatInputModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule
 ];
 
 @NgModule({
