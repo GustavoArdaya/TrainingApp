337d7f9a20418b39bd41cfe256ac31f471fd1fcc
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-10-12 10:46:32 +0200

    hide sidenav menu items depending on authstate

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,19 +1,35 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs-compat/Subscription';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.css']
 })
-export class SidenavListComponent implements OnInit{
+export class SidenavListComponent implements OnInit, OnDestroy{
+
+  isAuth = false;
+  authSubscription = new Subscription();
   
   @Output() sidenavClose = new EventEmitter();
+
+  constructor(private authService: AuthService) {
+    
+  }
   
   ngOnInit(): void {
+    this.authSubscription = this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    })
   }
 
   onClose() {
     this.sidenavClose.emit();
+  }
+
+  ngOnDestroy(): void {
+    this.authSubscription.unsubscribe();
   }
 
 }
