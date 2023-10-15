81b554b5832fc00af360a5368d45f4055c0c2f0f
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-10-15 08:48:01 +0200

    add mat table to past trainings

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -14,6 +14,7 @@ import { MatCardModule } from "@angular/material/card";
 import { MatSelectModule } from "@angular/material/select";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { MatDialogModule } from "@angular/material/dialog";
+import { MatTableModule } from "@angular/material/table";
 
 
 const MaterialModules = [
@@ -31,7 +32,8 @@ const MaterialModules = [
     MatCardModule,
     MatSelectModule,
     MatProgressSpinnerModule,
-    MatDialogModule
+    MatDialogModule,
+    MatTableModule
 ];
 
 @NgModule({
--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { StopTrainingComponent } from './stop-training.component';
 import { TrainingService } from '../training.service';
@@ -10,7 +10,6 @@ import { TrainingService } from '../training.service';
 })
 export class CurrentTrainingComponent implements OnInit{
   
-  @Output() trainingExit = new EventEmitter();
   progress = 0;
   timer: any;
 
@@ -30,6 +29,7 @@ export class CurrentTrainingComponent implements OnInit{
     this.timer = setInterval(()=> {
       this.progress += 1;
       if (this.progress >= 100) {
+        this.trainingService.completeExercise();
         clearInterval(this.timer);
       }
     }, step);
@@ -45,7 +45,7 @@ export class CurrentTrainingComponent implements OnInit{
 
     dialogRef.afterClosed().subscribe(result=> {
       if (result) {
-        this.trainingExit.emit();
+        this.trainingService.cancelExercise(this.progress);
       } else {
         this.startOrResumeTimer();
       }
