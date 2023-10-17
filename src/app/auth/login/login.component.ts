c9b6b4ab8d35d228f8f3ef6aa3a7e498fd89416d
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-10-17 12:12:29 +0200

    add db fetching of exercises to populate dropdown menu

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -14,7 +14,7 @@ export class LoginComponent implements OnInit{
   }
   
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    
   }
 
   onSubmit(form: NgForm) {
--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -1,23 +1,44 @@
 import { Exercise } from "./exercise.model";
 import { Subject } from "rxjs-compat/Subject";
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class TrainingService {
     
     exerciseChanged = new Subject<Exercise>();
+    exercisesChanged = new Subject<Exercise[]>();
 
-    private availableExercises: Exercise[] = [
-        { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
-        { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
-        { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
-        { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
-    ];
+    private availableExercises: Exercise[] = [];
 
     private runningExercise: Exercise;
     private exercises: Exercise[] = [];
 
+    constructor(private db: AngularFirestore) {
+        
+    }
 
-    getAvailableExercises() {
-        return this.availableExercises.slice();
+
+    fetchAvailableExercises() {
+        return this.db
+        .collection('availableExercises')
+        .snapshotChanges()
+        .pipe(
+          map((docArray) => {
+            return docArray.map(doc => {
+              return {
+                id: doc.payload.doc.id,
+                ...(doc.payload.doc.data() as Exercise)
+              };
+            });
+          })
+        )
+        .subscribe(exercises => {
+            this.availableExercises = exercises;
+            this.exercisesChanged.next([...this.availableExercises]);
+        });
     }
 
     startExercise(selectedId: string) {
